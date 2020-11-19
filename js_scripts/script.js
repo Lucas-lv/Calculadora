648bad50164fd488730fe94fb583c09cbff2eefb
Lucas-lv <lucasvigna2001@gmail.com>
2020-11-19 17:42:10 +0100

    boton información

--- js_scripts/script.js
+++ js_scripts/script.js
@@ -5,3 +5,24 @@ function Funcio() {
 function Funcio1() {
   window.location.assign("html/calculadoraCient.html");
 }
+
+function info(){
+
+  var modDate = new Date(document.lastModified)
+  var modYear = modDate.getYear()
+  if(modYear<1000) modYear+=1900
+  var txt = "";
+
+  txt += "Nombre del código del navegador: " + navigator.appCodeName + ",   ";
+  txt += "Nombre del navegador: " + navigator.appName + ",   ";
+  txt += "Versión del navegador: " + navigator.appVersion + ",   ";
+  txt += "Cookies habilitadas: " + navigator.cookieEnabled + ",   ";
+  txt += "Idioma del navegador: " + navigator.language + ",   ";
+  txt += "Navegador Online: " + navigator.onLine + ",   ";
+  txt += "Plataforma: " + navigator.platform + ",   ";
+  txt += "Encabezado de agente de usuario: " + navigator.userAgent + ",   ";
+  txt += "Última modificación del CV: " + modDate.getDate() + "/" + (modDate.getMonth()+1) + "/" + (modYear+"");
+
+  alert(txt);
+
+}
